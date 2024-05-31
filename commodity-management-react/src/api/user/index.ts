2e740952cb3e80bb02754135d4a7a20e84905bcd
chenwenzhuo <Chenwz1996@163.com>
2024-05-31 18:12:01 +0800

    定义用户相关TS类型和接口方法

---
+++ commodity-management-react/src/api/user/index.ts
@@ -0,0 +1,18 @@
+import request from "@/utils/request.ts";
+import {LoginForm, LoginResponse, UserInfoResponse} from "@/api/user/types.ts";
+
+//项目用户相关的请求地址
+enum API {
+  LOGIN = '/user/login',
+  USER_INFO = '/user/info',
+  LOGOUT = "/admin/acl/index/logout"
+}
+
+// 登录接方法
+export const reqLogin = (data: LoginForm) => request.post<any, LoginResponse>(API.LOGIN, data);
+
+// 获取用户信息方法
+export const reqUserInfo = () => request.get<any, UserInfoResponse>(API.USER_INFO);
+
+//退出登录
+export const reqLogout = () => request.post<any, any>(API.LOGOUT);
---
+++ commodity-management-react/src/api/user/types.ts
@@ -0,0 +1,45 @@
+//定义用户相关数据的ts类型
+//用户登录接口携带参数的ts类型
+export interface LoginForm {
+  username: string,
+  password: string
+}
+
+interface DataType {
+  token: string,
+}
+
+//定义全部接口返回数据都拥有ts类型
+export interface Response {
+  code: number,
+  message: string,
+  ok: boolean
+}
+
+//定义登录接口返回数据类型
+export interface LoginResponse {
+  code: number,
+  data: DataType,
+}
+
+//定义获取用户信息返回数据类型
+export interface UserInfo {
+  userId: number;
+  avatar: string;
+  username: string;
+  password: string;
+  desc: string;
+  roles: string[];
+  buttons: string[];
+  routes: string[];
+  token: string;
+}
+
+interface user {
+  checkUser: UserInfo
+}
+
+export interface UserInfoResponse extends Response {
+  code: number,
+  data: user,
+}
