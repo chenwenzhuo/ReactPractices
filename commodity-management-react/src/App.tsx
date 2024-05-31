ca146b2ef6e753a711f7470a05f1d42d5fb1132c
chenwenzhuo <Chenwz1996@163.com>
2024-05-31 16:17:19 +0800

    引入scss和mock

--- commodity-management-react/src/App.tsx
+++ commodity-management-react/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-    return (
-        <>
-            <h1>商品管理系统</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>商品管理系统</h1>
+    </>
+  )
 }
 
 export default App
--- commodity-management-react/vite.config.ts
+++ commodity-management-react/vite.config.ts
@@ -1,12 +1,18 @@
-import {defineConfig} from 'vite'
+import {ConfigEnv, defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {viteMockServe} from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command}: ConfigEnv) => ({
+  plugins: [
+    react(),
+    viteMockServe({
+      localEnabled: command === 'serve',
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
     },
   }
-})
+}));
