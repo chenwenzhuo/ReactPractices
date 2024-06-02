5c569b099c5b3acb83df571311e79ae6341f0016
chenwenzhuo <Chenwz1996@163.com>
2024-06-02 16:24:37 +0800

    直接访问/admin时，重定向到/admin/home

--- commodity-management-react/src/pages/layout/AppLayout.tsx
+++ commodity-management-react/src/pages/layout/AppLayout.tsx
@@ -1,14 +1,24 @@
-import {FC} from "react";
+import {FC, useEffect} from "react";
+import {Outlet, useLocation, useNavigate} from "react-router-dom";
 import {Layout} from 'antd';
 
 import './AppLayout.scss';
 import Logo from "@/components/Logo/Logo.tsx";
 import {SiderMenu} from "@/components/SiderMenu/SiderMenu.tsx";
-import {Outlet} from "react-router-dom";
 
 const {Header, Footer, Sider, Content} = Layout;
 
 const AppLayout: FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // 当用户在地址栏直接输入 /admin 路径时，重定向到首页
+    if (location.pathname === '/admin') {
+      navigate('/admin/home');
+    }
+  }, []);
+
   return (
     <Layout className={"home-component"}>
       <Sider width={"360px"} className={"home-sider"}>
