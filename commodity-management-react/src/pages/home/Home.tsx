625e7c11d7e479d9a336f8cf2c7636b5f9157766
chenwenzhuo <Chenwz1996@163.com>
2024-06-16 15:39:47 +0800

    完成首页

--- commodity-management-react/src/pages/home/Home.tsx
+++ commodity-management-react/src/pages/home/Home.tsx
@@ -1,8 +1,29 @@
 import {FC} from "react";
+import {useAppSelector} from "@/redux/hooks.ts";
+import {selectAllUserState} from "@/redux/userSlice.ts";
+import {Card} from "antd";
+import {getTime} from "@/utils/time.ts";
+import welcomeImg from "@/assets/icons/welcome.svg";
+import './Home.scss';
 
 const Home: FC = () => {
+  const {username, avatar} = useAppSelector(selectAllUserState);
+
   return (
-    <div>首页</div>
+    <main className={"home-component"}>
+      <Card style={{width: "100%"}}>
+        <div className={"card-content"}>
+          <img src={avatar} alt={""}/>
+          <div className={"text"}>
+            <p className={"greeting"}>{username}，{getTime()}好!</p>
+            <p className={"platform"}>商品管理经营平台</p>
+          </div>
+        </div>
+      </Card>
+      <div className={"welcome-img-wrapper"}>
+        <img src={welcomeImg} alt={""}/>
+      </div>
+    </main>
   );
 };
 
